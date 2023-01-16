f2dd921c4c4db3bb4cc36dc9db3343f72d2cf321
Chaerin2 <94672670+Chaerin2@users.noreply.github.com>
2023-01-16 19:11:20 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,31 +1,5 @@
 from hanspell import spell_checker
 
-f = open('1.txt','r',encoding='utf-8')
-text = f.readlines()
-f.close()
-
-new_txt = []
-
-for i in text:
-  new_txt.append(i.replace('\n',""))
-  str_txt=""
-for i in new_txt:
-  str_txt = str_txt +i +""
-  
-for i in range(0,50):
-  globals()['sent{}'.format(i)] = str_txt[500*i:500*i+499]
-  
- spelled_sent = spell_checker.check(globals()['sent'{}.format(i)])
- checked_Sent = spelled_sent.checked
-  
- f = open('result.txt','a+',encoding = 'utf-8')
- f.write(checked_sent)
- f.close()
-
-
-
-------------------------------------------
-
 fp = open('1.txt','r',encoding='cp949')
 text = fp.read()
 fp.close()
